a57d1cf9e23f941db23cf18d142b7bce5c2046d3
zhhbstudio <zhhb@zhhb.studio>
2021-08-01 03:26:18 +0000

    feat：增加 table 示例页面

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
-  <router-view />
+  <n-config-provider :theme="darkTheme" :locale="zhCN" :date-locale="dateZhCN">
+    <router-view />
+  </n-config-provider>
 </template>
 
 <script setup>
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+
+import { NConfigProvider, zhCN, dateZhCN } from 'naive-ui'
 </script>
 
 <style>
 html,
 body {
-  margin:0;
+  margin: 0;
   padding: 0;
   height: 100%;
   min-width: 1260px;
   min-height: 550px;
 }
 #app {
-  position:relative;
+  position: relative;
   width: 100%;
   height: 100%;
 }
--- src/views/table/table.vue
+++ src/views/table/table.vue
@@ -1,7 +1,50 @@
-<template>TABLE</template>
+<template>
+  <n-data-table
+    ref="table"
+    :columns="columns"
+    :data="data"
+    :pagination="pagination"
+    :locale="zhCN"
+  />
+</template>
 
 <script setup>
+import { reactive } from "vue";
+import { NDataTable, zhCN } from 'naive-ui'
+const columns = [
+  {
+    title: 'Name',
+    key: 'name'
+  },
+  {
+    title: 'Age',
+    key: 'age'
+  },
+  {
+    title: 'Address',
+    key: 'address'
+  }
+]
+const data = Array.apply(null, { length: 46 }).map((_, index) => ({
+  key: index,
+  name: `Edward King ${index + 1}`,
+  age: 32,
+  address: `London, Park Lane no. ${index}`
+}))
 
+let pagination = reactive({
+  page: 1,
+  pageSize: 5,
+  showSizePicker: true,
+  pageSizes: [5, 10, 20],
+  onChange: (page) => {
+    pagination.page = page
+  },
+  onPageSizeChange: (pageSize) => {
+    pagination.pageSize = pageSize
+    pagination.page = 1
+  }
+})
 </script>
 
 <style scoped>
