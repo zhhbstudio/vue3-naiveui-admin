f186673ff5becceff5664fd923e33ceeed7d0280
zhhbstudio <zhhb@zhhb.studio>
2023-03-12 11:20:15 +0000

    chore: 更新依赖版本

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,14 @@ module.exports = {
     'eslint:recommended'
   ],
   rules: {
+    'vue/multi-word-component-names': 'off',
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 5
+      },      
+      "multiline": {
+        "max": 3
+      }
+    }]
   }
 }
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   "extends": [
     "stylelint-config-standard-scss",
-    "stylelint-config-recommended-vue/scss",
-    "stylelint-config-rational-order-mod"
+    "stylelint-config-recommended-vue/scss"
   ],
   "plugins": [
     "stylelint-scss",
-    "stylelint-order",
-    "stylelint-config-rational-order-mod/plugin"
+    "stylelint-order"
   ]
 }
